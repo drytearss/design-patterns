7e53a76691f75d83b859a678dcebc8a844d0c618
drytearss <kirillufo@gmail.com>
2024-06-09 10:52:27 +0300

    small fix adapter

--- internal/structural/adapter/adapter.go
+++ internal/structural/adapter/adapter.go
@@ -2,26 +2,31 @@ package adapter
 
 import "fmt"
 
+// В большинстве случаев интерфейс определяем на стороне потребетиля, а не конкретной реализации
 type Charger interface {
 	ChargeFromLightning()
 }
 
+// Код который писали мы (условно)
 type LightningCharger struct{}
 
 func (l *LightningCharger) ChargeFromLightning() {
 	fmt.Println("Charge from lightning...")
 }
 
+// Код из сторонней библиотеки (условно)
 type TypeCCharger struct{}
 
 func (t *TypeCCharger) ChargeFromTypeC() {
 	fmt.Println("Charge from Type-C...")
 }
 
+// Созданный нами адаптер
 type TypeCAdapter struct {
 	TypeCCharger *TypeCCharger
 }
 
 func (a *TypeCAdapter) ChargeFromLightning() {
+	// Тут можно добавить свою логику
 	a.TypeCCharger.ChargeFromTypeC()
 }
